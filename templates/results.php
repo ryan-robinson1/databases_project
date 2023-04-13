041cbb7e83f25a516e6e181513060dbade1d8f26
ryan-robinson1 <69409000+ryan-robinson1@users.noreply.github.com>
2023-04-12 22:17:26 -0400

    Results template

--- templates/results.php
+++ templates/results.php
@@ -7,6 +7,17 @@
     <title>Hoo's Reviews</title>
     <!-- Add Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .light-grey-border {
+            border: 5px solid #A9A9A9;
+        }
+
+        .list-group-item {
+            margin-top: 10px;
+            margin-bottom: 10px;
+            border: 3px solid #D3D3D3 !important;
+        }
+    </style>
 </head>
 
 <body>
@@ -26,16 +37,26 @@
         </div>
     </nav>
     <div class="container">
-        <div class="mt-5">
-            <h5 class="text-left">Results for: CS 4750</h5>
-        </div>
-        <div class="row mt-3">
+        <div class="row mt-5">
             <div class="col-md-12">
-                <div class="list-group">
-                    <button type="button" class="list-group-item list-group-item-action">
-                        CS 4750 - Database Systems
-                    </button>
-                    <!-- Add more buttons for additional search results -->
+
+
+                <h5 class="card-title text-left">Results for: CS 47..</h5>
+                <div class="card light-grey-border">
+                    <div class="card-body">
+                        <div class="list-group ">
+                            <button type="button" class="list-group-item list-group-item-action">
+                                CS 4750 - Database Systems
+                            </button>
+                            <button type="button" class="list-group-item list-group-item-action">
+                                CS 4700 - Intro to Kubernetes
+                            </button>
+                            <button type="button" class="list-group-item list-group-item-action">
+                                CS 4790 - Database Systems 2
+                            </button>
+                            <!-- Add more buttons for additional search results -->
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
